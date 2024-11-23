94d277c69fcc395c8629865cac85ac05eb99dced
mirsazzathossain@gmail.com <kishor25@g.skku.skku.edu>
2024-11-23 18:52:39 +0600

    Refactor code!

--- fed_utils.py
+++ fed_utils.py
@@ -1,15 +1,38 @@
+import random
+
 import torch
 import torch.nn as nn
-import random
+
 from robustbench.data import load_cifar10c, load_cifar100c
-    
+
+
 @torch.no_grad()
-def ema_update_model(model_to_update, model_to_merge, momentum, device, update_all=False):
+def ema_update_model(
+    model_to_update, model_to_merge, momentum, device, update_all=False
+):
+    """
+    Updates the model_to_update with the model_to_merge using exponential moving average.
+
+    Args:
+        model_to_update (nn.Module): Model to update.
+        model_to_merge (nn.Module): Model to merge.
+        momentum (float): Momentum for the EMA.
+        device (torch.device): Device to move the model to.
+        update_all (bool): Whether to update all parameters or only the trainable ones.
+
+    Returns:
+        nn.Module: Updated model.
+    """
     if momentum < 1.0:
-        for param_to_update, param_to_merge in zip(model_to_update.parameters(), model_to_merge.parameters()):
+        for param_to_update, param_to_merge in zip(
+            model_to_update.parameters(), model_to_merge.parameters()
+        ):
             if param_to_update.requires_grad or update_all:
-                param_to_update.data = momentum * param_to_update.data + (1 - momentum) * param_to_merge.data.to(device)
+                param_to_update.data = momentum * param_to_update.data + (
+                    1 - momentum
+                ) * param_to_merge.data.to(device)
     return model_to_update
+
 
 def split_indices_into_batches(total_indices, batch_size):
     """
@@ -23,72 +46,166 @@ def split_indices_into_batches(total_indices, batch_size):
         list: List of batches, where each batch is a list of indices.
     """
     num_full_batches = total_indices // batch_size
-    remaining_indices = total_indices % batch_size
+    # remaining_indices = total_indices % batch_size
 
     shuffled_indices = list(range(total_indices))
     random.shuffle(shuffled_indices)
     index_batches = []
 
     for batch_idx in range(num_full_batches):
-        batch_indices = shuffled_indices[batch_idx * batch_size: (batch_idx + 1) * batch_size]
+        batch_indices = shuffled_indices[
+            batch_idx * batch_size : (batch_idx + 1) * batch_size
+        ]
         index_batches.append(batch_indices)
 
     return index_batches
 
+
 def get_available_corruptions(dataset, max_use_count):
+    """
+    Returns the list of corruptions that can be used.
+
+    Args:
+        dataset (dict): Dictionary containing the dataset.
+        max_use_count (int): Maximum number of times a corruption can be used.
+
+    Returns:
+        list: List of corruptions that can be used.
+    """
     available_corruptions = []
     for corruption_type in dataset.keys():
-        if dataset[corruption_type]['use_count'] < max_use_count:
+        if dataset[corruption_type]["use_count"] < max_use_count:
             available_corruptions.append(corruption_type)
     return available_corruptions
 
+
 def get_dataset(cfg, severity, dataset_name):
+    """
+    Loads the dataset with the given corruptions and severity.
+
+    Args:
+        cfg (dict): Configuration dictionary.
+        severity (int): Severity of the corruptions.
+        dataset_name (str): Name of the dataset.
+
+    Returns:
+        dict: Dictionary containing the dataset.
+    """
     dataset = {}
-    for i_c, corruption_type in enumerate(cfg.CORRUPTION.TYPE):
+    for _, corruption_type in enumerate(cfg.CORRUPTION.TYPE):
         dataset[corruption_type] = {}
-        if dataset_name == 'cifar10':
-            x_test, y_test = load_cifar10c(cfg.CORRUPTION.NUM_EX, severity, cfg.MISC.DATA_DIR, True, [corruption_type]) 
-        elif dataset_name == 'cifar100':
-            x_test, y_test = load_cifar100c(cfg.CORRUPTION.NUM_EX, severity, cfg.MISC.DATA_DIR, True, [corruption_type])            
-        dataset[corruption_type]['all_x'] = x_test
-        dataset[corruption_type]['all_y'] = y_test
-        dataset[corruption_type]['indices'] = split_indices_into_batches(len(x_test), cfg.MISC.BATCH_SIZE)
-        dataset[corruption_type]['use_count'] = 0
-        
+        if dataset_name == "cifar10":
+            x_test, y_test = load_cifar10c(
+                cfg.CORRUPTION.NUM_EX,
+                severity,
+                cfg.MISC.DATA_DIR,
+                True,
+                [corruption_type],
+            )
+        elif dataset_name == "cifar100":
+            x_test, y_test = load_cifar100c(
+                cfg.CORRUPTION.NUM_EX,
+                severity,
+                cfg.MISC.DATA_DIR,
+                True,
+                [corruption_type],
+            )
+        dataset[corruption_type]["all_x"] = x_test
+        dataset[corruption_type]["all_y"] = y_test
+        dataset[corruption_type]["indices"] = split_indices_into_batches(
+            len(x_test), cfg.MISC.BATCH_SIZE
+        )
+        dataset[corruption_type]["use_count"] = 0
+
     return dataset
 
+
 def assign_clients(num_clients, domains):
+    """
+    Assigns clients to domains.
+
+    Args:
+        num_clients (int): Total number of clients.
+        domains (list): List of domains.
+
+    Returns:
+        dict: Dictionary containing the assigned clients for each domain.
+    """
     clients = list(range(num_clients))
     num_domains = len(domains)
     random.shuffle(clients)
-    distributed_clients = {domains[i]: clients[i::num_domains] for i in range(num_domains)}
+    distributed_clients = {
+        domains[i]: clients[i::num_domains] for i in range(num_domains)
+    }
     return distributed_clients
 
+
 def get_available_domains(selection_count, domains_per_timestep, max_selection_count):
-    available_domains = [domain for domain, count in selection_count.items() if count < max_selection_count//2]
+    """
+    Returns the list of domains that can be used.
+
+    Args:
+        selection_count (dict): Dictionary containing the selection count for each domain.
+        domains_per_timestep (int): Number of domains per timestep.
+        max_selection_count (int): Maximum number of times a domain can be selected.
+
+    Returns:
+        list: List of domains that can be used.
+    """
+    available_domains = [
+        domain
+        for domain, count in selection_count.items()
+        if count < max_selection_count // 2
+    ]
     if len(available_domains) < domains_per_timestep:
-        sorted_domains = sorted(selection_count.items(), key=lambda item: item[1])  # Sort by selection count
-        additional_domains = [domain for domain, count in sorted_domains if domain not in available_domains]
-        available_domains.extend(additional_domains[:domains_per_timestep - len(available_domains)])
+        sorted_domains = sorted(
+            selection_count.items(), key=lambda item: item[1]
+        )  # Sort by selection count
+        additional_domains = [
+            domain
+            for domain, count in sorted_domains
+            if domain not in available_domains
+        ]
+        available_domains.extend(
+            additional_domains[: domains_per_timestep - len(available_domains)]
+        )
 
     return available_domains
 
+
 def create_schedule_niid(num_clients, timesteps, domains, temporal_h, spatial_h):
+    """
+    Creates a non-IID schedule for the clients.
+
+    Args:
+        num_clients (int): Total number of clients.
+        timesteps (int): Total number of timesteps.
+        domains (list): List of domains.
+        temporal_h (float): Temporal heterogeneity.
+        spatial_h (float): Spatial heterogeneity.
+
+    Returns:
+        list: List of domains assigned to each client at each timestep.
+    """
     domain_per_step = int(spatial_h * num_clients)
-    time_interval = int(1/temporal_h)
+    time_interval = int(1 / temporal_h)
     assert time_interval > 0 and time_interval <= timesteps
     schedule = []
     selection_count = {domain: 0 for domain in domains}
-    max_selection_count = timesteps*domain_per_step//(len(domains))
+    max_selection_count = timesteps * domain_per_step // (len(domains))
 
     # Create the schedule and client assignments
     for t in range(timesteps):
         if t % time_interval == 0:
-            available_domains = get_available_domains(selection_count, domain_per_step, max_selection_count)
+            available_domains = get_available_domains(
+                selection_count, domain_per_step, max_selection_count
+            )
             selected_domains = random.sample(available_domains, domain_per_step)
             # Update selection count
             for domain in selected_domains:
-                selection_count[domain] += int(num_clients * time_interval/domain_per_step)
+                selection_count[domain] += int(
+                    num_clients * time_interval / domain_per_step
+                )
             # Add to schedule
             schedule.append(selected_domains)
 
@@ -100,32 +217,66 @@ def create_schedule_niid(num_clients, timesteps, domains, temporal_h, spatial_h)
         distributed_clients = assign_clients(num_clients, selected_domains)
         for domain, assigned_clients in distributed_clients.items():
             for client in assigned_clients:
-                clients_schedule[client].extend([domain]*time_interval)
+                clients_schedule[client].extend([domain] * time_interval)
     return clients_schedule
 
+
 def create_schedule_iid(num_clients, timesteps, domains, temporal_h):
-    time_interval = int(1/temporal_h)
-    max_count = timesteps//len(domains)
+    """
+    Creates an IID schedule for the clients.
+
+    Args:
+        num_clients (int): Total number of clients.
+        timesteps (int): Total number of timesteps.
+        domains (list): List of domains.
+        temporal_h (float): Temporal heterogeneity.
+
+    Returns:
+        list: List of domains assigned to each client at each timestep.
+    """
+    time_interval = int(1 / temporal_h)
+    max_count = timesteps // len(domains)
     use_count = {domain: 0 for domain in domains}
     schedule = []
     last_selection = None
-    
+
     for t in range(timesteps):
         if t % time_interval == 0:
-            available_domains = [domain for domain, count in use_count.items() if last_selection != domain and  count < max_count]
+            available_domains = [
+                domain
+                for domain, count in use_count.items()
+                if last_selection != domain and count < max_count
+            ]
             last_selection = random.choice(available_domains)
-            schedule.extend([last_selection]*min(time_interval, max_count - use_count[last_selection]))
-            use_count[last_selection] += min(time_interval, max_count - use_count[last_selection])
+            schedule.extend(
+                [last_selection]
+                * min(time_interval, max_count - use_count[last_selection])
+            )
+            use_count[last_selection] += min(
+                time_interval, max_count - use_count[last_selection]
+            )
 
     client_schedule = [schedule for i in range(num_clients)]
     return client_schedule
 
 
 def cosine_similarity(bn_params1, bn_params2):
+    """
+    Computes the cosine similarity between the batch normalization parameters of two models.
+
+    Args:
+        bn_params1 (dict): Dictionary containing the batch normalization parameters of model 1.
+        bn_params2 (dict): Dictionary containing the batch normalization parameters of model 2.
+
+    Returns:
+        float: Cosine similarity between the batch normalization parameters.
+    """
     assert bn_params1.keys() == bn_params2.keys(), "Keys must match"
     similarities = []
     for layer_name in bn_params1:
-        similarity = nn.functional.cosine_similarity(bn_params1[layer_name], bn_params2[layer_name], dim = 0)
-        similarities.append(similarity.item())  
-        
-    return sum(similarities)/len(similarities)
+        similarity = nn.functional.cosine_similarity(
+            bn_params1[layer_name], bn_params2[layer_name], dim=0
+        )
+        similarities.append(similarity.item())
+
+    return sum(similarities) / len(similarities)
